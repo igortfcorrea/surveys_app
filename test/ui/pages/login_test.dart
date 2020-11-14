0eadd722c927bde4214ada6ec056a98cd2f24d4b
Igor Correa <igortfcorrea@gmail.com>
2020-11-14 12:48:34 -0300

    ensure onChange call validate methods

--- test/ui/pages/login_test.dart
+++ test/ui/pages/login_test.dart
@@ -1,13 +1,25 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
-import 'package:surveys_app/ui/pages/login_page.dart';
+import 'package:surveys_app/ui/pages/login/login_page.dart';
+import 'package:surveys_app/ui/pages/login/login_presenter.dart';
+
+class LoginPresenterSpy extends Mock implements LoginPresenter {}
 
 void main() {
+  LoginPresenter presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = LoginPresenterSpy();
+    final loginPage = MaterialApp(home: LoginPage(presenter));
+    await tester.pumpWidget(loginPage);
+  }
+
   testWidgets('Should load with correct initial state',
       (WidgetTester tester) async {
-    final loginPage = MaterialApp(home: LoginPage());
-    await tester.pumpWidget(loginPage);
+    await loadPage(tester);
 
     final emailTextChildren = find.descendant(
         of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
@@ -23,5 +35,18 @@ void main() {
 
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, null);
+  });
+
+  testWidgets('Should call validate with correct values',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    final email = faker.internet.email();
+    await tester.enterText(find.bySemanticsLabel('Email'), email);
+    verify(presenter.validateEmail(email));
+
+    final password = faker.internet.password();
+    await tester.enterText(find.bySemanticsLabel('Senha'), password);
+    verify(presenter.validatePassword(password));
   });
 }
