8966a2f1850473eabff6d134a9388a784b7e6ad9
Igor Correa <igortfcorrea@gmail.com>
2020-11-26 15:41:09 -0300

    ensure StreamLoginPresenter calls Authentication with correct values

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-
 import 'package:meta/meta.dart';
+
+import '../../domain/usecases/usecases.dart';
 
 import '../protocols/protocols.dart';
 
@@ -18,6 +19,7 @@ class LoginState {
 
 class StreamLoginPresenter {
   final Validation validation;
+  final Authentication authentication;
   final _controller = StreamController<LoginState>.broadcast();
 
   var _state = LoginState();
@@ -29,7 +31,8 @@ class StreamLoginPresenter {
   Stream<bool> get isFormValidStream =>
       _controller.stream.map((state) => state.isFormValid).distinct();
 
-  StreamLoginPresenter({@required this.validation});
+  StreamLoginPresenter(
+      {@required this.validation, @required this.authentication});
 
   void _update() => _controller.add(_state);
 
@@ -44,5 +47,10 @@ class StreamLoginPresenter {
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     _update();
+  }
+
+  Future<void> auth() async {
+    await authentication.auth(
+        AuthenticationParams(email: _state.email, password: _state.password));
   }
 }
--- test/ui/presentation/presenters/stream_login_presenter_test.dart
+++ test/ui/presentation/presenters/stream_login_presenter_test.dart
@@ -5,11 +5,16 @@ import 'package:test/test.dart';
 import 'package:surveys_app/presentation/protocols/protocols.dart';
 import 'package:surveys_app/presentation/presenters/presenters.dart';
 
+import 'package:surveys_app/domain/usecases/usecases.dart';
+
 class ValidationSpy extends Mock implements Validation {}
+
+class AuthenticationSpy extends Mock implements Authentication {}
 
 void main() {
   StreamLoginPresenter sut;
   ValidationSpy validation;
+  AuthenticationSpy authentication;
   String email;
   String password;
 
@@ -23,7 +28,9 @@ void main() {
 
   setUp(() {
     validation = ValidationSpy();
-    sut = StreamLoginPresenter(validation: validation);
+    authentication = AuthenticationSpy();
+    sut = StreamLoginPresenter(
+        validation: validation, authentication: authentication);
     email = faker.internet.email();
     password = faker.internet.password();
     mockValidation();
@@ -107,5 +114,16 @@ void main() {
     sut.validateEmail(email);
     await Future.delayed(Duration.zero);
     sut.validatePassword(password);
+  });
+
+  test('Should call Authentication with correct values', () async {
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    await sut.auth();
+
+    verify(authentication
+            .auth(AuthenticationParams(email: email, password: password)))
+        .called(1);
   });
 }
