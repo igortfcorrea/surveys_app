42b19a936a509667e2633e6b7f678dca70e4e135
Igor Correa <igortfcorrea@gmail.com>
2020-11-26 21:27:53 -0300

    ensure LoginValidationFactory returns correct validations

--- lib/validation/validators/email_validation.dart
+++ lib/validation/validators/email_validation.dart
@@ -1,7 +1,10 @@
+import 'package:equatable/equatable.dart';
 import 'package:surveys_app/validation/protocols/field_validation.dart';
 
-class EmailValidation implements FieldValidation {
+class EmailValidation extends Equatable implements FieldValidation {
   final String field;
+
+  List get props => [field];
 
   EmailValidation(this.field);
 
